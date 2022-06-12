d665596ab2b13c8dfc921577a9f7b69c573bd70e
Pavel Sergeev <sergeevpasha90@gmail.com>
2022-06-13 00:23:30 +0700

    fix: remove routes

--- src/Providers/DPDServiceProvider.php
+++ src/Providers/DPDServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SergeevPasha\DPD\Providers;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use SergeevPasha\DPD\Libraries\DPDClient;
 
@@ -28,7 +27,6 @@ class DPDServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerRoutes();
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'dpd');
         if ($this->app->runningInConsole()) {
             $this->publishes(
@@ -38,33 +36,5 @@ class DPDServiceProvider extends ServiceProvider
                 'config'
             );
         }
-    }
-
-    /**
-     * Register routes
-     *
-     * @return void
-     */
-    protected function registerRoutes(): void
-    {
-        Route::group(
-            $this->routeConfiguration(),
-            function () {
-                $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-            }
-        );
-    }
-
-    /**
-     * Routes Configuration
-     *
-     * @return array
-     */
-    protected function routeConfiguration(): array
-    {
-        return [
-            'prefix'     => config('dpd.prefix'),
-            'middleware' => config('dpd.middleware'),
-        ];
     }
 }
